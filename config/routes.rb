fd88d5bcf8b4f5f129c42d4a9d44769868bb8a46
Karol Bąk <kukicola@gmail.com>
2022-12-12 18:40:47 +0100

    sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require "sidekiq/web"
+
 module ShinyGems
   class Routes < Hanami::Routes
+    mount Sidekiq::Web, at: "/admin/sidekiq"
+
     root to: "pages.index"
     get "auth/:provider/callback", to: "session.create"
     get "auth/destroy", to: "session.destroy"
