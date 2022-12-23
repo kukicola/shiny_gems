3a7d0db9e716efa79be674864809cb306c7e958b
Karol Bąk <kukicola@gmail.com>
2022-12-23 18:55:12 +0100

    Revert "remove extention"
    This reverts commit 9bb3a69c26cbf5e7006f983c4773084fd8f606ca.

--- config/providers/persistence.rb
+++ config/providers/persistence.rb
@@ -4,7 +4,9 @@ Hanami.app.register_provider :persistence, namespace: true do
   prepare do
     require "rom"
 
-    config = ROM::Configuration.new(:sql, target["settings"].database_url, max_connections: 10)
+    config = ROM::Configuration.new(:sql, target["settings"].database_url, max_connections: 10, extensions: [:connection_validator])
+
+    config.gateways[:default].connection.pool.connection_validation_timeout = 1600
 
     register "config", config
     register "db", config.gateways[:default].connection
