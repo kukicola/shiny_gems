c9f1f18c8d9505dc02b59e054202acb7117a7798
Karol Bąk <kukicola@gmail.com>
2023-01-29 22:30:14 +0100

    param validation

--- slices/web/actions/gems/show.rb
+++ slices/web/actions/gems/show.rb
@@ -6,6 +6,12 @@ module Web
       class Show < Web::Action
         include Deps["repositories.gems_repository"]
 
+        before :validate_params!
+
+        params do
+          required(:id).filled(:integer)
+        end
+
         def handle(request, response)
           gem = gems_repository.by_id(request.params[:id], with: {repo: [:issues, :gems]})
           raise NotFoundError unless gem
