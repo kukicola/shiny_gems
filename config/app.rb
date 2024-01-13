48a37f94350578671e8e7cc9c94cf04e68dacc86
Karol Bąk <kukicola@gmail.com>
2024-01-13 16:40:40 +0100

    reenable session

--- config/app.rb
+++ config/app.rb
@@ -6,11 +6,11 @@ module ShinyGems
   class App < Hanami::App
     config.shared_app_component_keys += ["sentry", "persistence.rom"]
 
-    # config.actions.sessions = :cookie, {
-    #   key: "session",
-    #   secret: settings.session_secret,
-    #   expire_after: 60 * 60 * 24 * 365
-    # }
+    config.actions.sessions = :cookie, {
+      key: "session",
+      secret: settings.session_secret,
+      expire_after: 60 * 60 * 24 * 365
+    }
 
     config.actions.content_security_policy[:form_action] += " https://github.com"
     config.actions.content_security_policy[:script_src] += " https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js https://static.cloudflareinsights.com/beacon.min.js/"
